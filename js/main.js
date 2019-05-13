4daa2c293ab5dabc9579af1fbe89cfdc4af8558f
Nikita <karelison97@gmail.com>
2019-05-13 22:49:44 +0300

    section advantages & news

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-	$('.slider-inner').slick({
+	$('.slider-inner, .news__slider-inner').slick({
 		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
 		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
 		infinite: false,
